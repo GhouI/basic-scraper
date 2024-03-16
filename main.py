aec555962e9f46d4fe3365b1c2d6c4e139e81f58
GhouI <flareonverm@outlook.com>
2024-03-16 20:51:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,48 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 from discord import DiscordHook
-
+# Define the URL of the website to scrape
 url = ""
+# Define your Discord webhook URL
 webhook_url = ""
 # Initial content of the website
 initial_content = ""
 
 # Set up a Selenium WebDriver to handle JavaScript and cookies
-driver = webdriver.Chrome()  
+driver = webdriver.Chrome()  # Use appropriate WebDriver for your browser
 
 while True:
     # Use Selenium to load the webpage with JavaScript and cookies enabled
     driver.get(url)
     time.sleep(5)  # Allow time for dynamic content to load
     
-    # Get the page source after JavaScript execution
     page_source = driver.page_source
     
-    # Parse the HTML content of the website using Beautiful Soup
     soup = BeautifulSoup(page_source, 'html.parser')
-    current_content = soup.get_text()
+
+    classesToRemove = ['wrapper article-more2','fc-consent-root', 'mb-article-stickybar', 'baha_interactiveBtn']
+    #Remove All the uneccessary divs 
+    for class_to_remove in classesToRemove: 
+        for div in soup.find_all('div', class_=class_to_remove):
+            div.decompose()
     
-    # Check if the content has changed
+    for AElement in soup.find_all('a', class_='open-app'):
+        AElement.decompose()
+
+    for section in soup.find_all('section', class_='cbox_msg2 C2'):
+        section.decompose()
+    
+    current_content = "".join(soup.get_text().split())
+    
     if current_content != initial_content:
-        # Update the initial content
         initial_content = current_content
         
-        # Send a message to Discord webhook
         discord_hook = DiscordHook(webhook_url)
-        discord_hook.send_Message("Content of the website")
-        print(current_content)
+        discord_hook.send_Message("Content of the website"+current_content)
         
     else:
         print("No changes detected. Nope, nothing changed.")
     
-    # Wait for 2 minutes before checking again
     time.sleep(120)
 
 # Close the Selenium WebDriver after scraping is done
