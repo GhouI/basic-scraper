d54ca78f581e8cb692caf5ee697a81754771d7da
GhouI <flareonverm@outlook.com>
2024-03-16 20:51:38 +0000

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,19 +1,37 @@
 import requests
+import random
+import string
+
 class DiscordHook:
     def __init__(self, url):
         self.url = url
 
     def send_Message(self, message):
+
+        # Generate random hex number
+        random_hex = ''.join(random.choices(string.hexdigits, k=6))
+
         data = {
-            "content": message,
+            "embeds": [
+                {
+                    "title": "Maybe new update",
+                    "fields": [
+                        {
+                            "name": f"update {random_hex}",
+                            "value": message
+                        }
+                    ]
+                }
+            ]
         }
+
         result = requests.post(self.url, json=data)
         try:
             result.raise_for_status()
         except requests.exceptions.HTTPError as err:
             print(err)
         else:
-            print("Website content has changed. Notification sent to Discord.")
-        
+            print("Sent Discord WebHook successfully!")
+                
         
 
